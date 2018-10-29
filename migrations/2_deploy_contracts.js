7716989002ced5bef58da17e3471d6ab701a7117
Nick Ward <n.ward13@yahoo.com>
2018-10-29 13:17:09 -0600

    Remove gas comparison

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var Channels = artifacts.require("./Channels.sol");
-var GasTesterChannels = artifacts.require("./GasTesterChannels.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(Channels);
-    deployer.deploy(GasTesterChannels);
 };
